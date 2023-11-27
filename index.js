38e3f3dbca29acfc4f3a3ba08d481ae13d79eb19
Mauricio <stb539@gmail.com>
2023-11-27 13:39:55 -0300

    process

--- index.js
+++ index.js
@@ -1,86 +1,80 @@
 import puppeteer from "puppeteer"
-
 ;(async () => {
-  // Inicie el navegador y abra una nueva página en blanco.
-  // const browser = await puppeteer.launch()
-
-  // nueva forma
-  const browser = await puppeteer.launch({ headless: "new" })
-  // const browser = await puppeteer.launch({ headless: false }) // abre el navegador y hace operacion
-  // const browser = await puppeteer.launch({ headless: true }) // no abre el navegador y hace operacion
-
-  const page = await browser.newPage()
-
-  // Navegar por la página a una URL ej https://developer.chrome.com/
-  // await page.goto("https://www.google.cl/")
-  // await page.goto("https://developer.chrome.com/")
-  await page.goto("https://www.amazon.es/") // aveces cokies, otras imagen
-
-  // Establecer tamaño de pantalla
-  await page.setViewport({ width: 1080, height: 1024 })
-
-  // captura de pantalla
-  // await page.screenshot({ path: 'amazon1.jpg' })
-  // await page.screenshot({ path: "chrome8.jpg" })
-
-  // Escribe en el cuadro de búsqueda
-  // await page.type("#APjFqb", "automate beyond recorder")
-  // await page.type(".search-box__input", "automate beyond recorder")
-  await page.type("#twotabsearchtextbox", "libros de javascript") // acá se cae aveces por uso de cokies
-  // await page.screenshot({ path: "amazon16.jpg" })
-
-  //Espere y haga clic en el primer resultado.
-  // const searchResultSelector = ".search-box__link"
-  // await page.waitForSelector(searchResultSelector) // data-component-type="s-search-result"
-  // await page.click('.gNO89b')
-
-  await page.click(".nav-search-submit input")
-  await page.waitForSelector("[data-component-type=s-search-result]") // esperar la carga del componente
-  await page.screenshot({ path: "amazon18.jpg" })
-  // await page.wait(3000) // esperar 3 segundos // obsoleto
-  const enlaces = await page.evaluate(() => {
-    const elements = document.querySelectorAll(
-      "[data-component-type=s-search-result] h2 a"
-    )
-
-    const links = []
-    for (let element of elements) {
-      links.push(element.href)
-    }
-    return links
-  })
-  console.log(enlaces.length) // numero de enlaces ej: 20
-
-  const books = []
-  for (let enlace of enlaces) {
-    await page.goto(enlace)
-    // await page.waitForDevicePrompt(1000) // waitFor
-    await page.waitForSelector("#productTitle")
-
-    const book = await page.evaluate(() => {
-      const tmp = {}
-      tmp.title = document.querySelector("#productTitle").innerHTML
-      tmp.author = document.querySelector(".author a").innerHTML
-      return tmp
+  try {
+    console.log("Abriendo el navegador......")
+    // Inicie el navegador y abra una nueva página en blanco.
+    const browser = await puppeteer.launch({
+      headless: "new",
+      /* args: ["--disable-setuid-sandbox"],
+      ignoreHTTPSErrors: true, */
     })
-    books.push(book)
+
+    const page = await browser.newPage()
+
+    console.log('entrando a google...')
+    await page.goto("https://www.google.cl/")
+
+    // Establecer tamaño de pantalla
+    await page.setViewport({ width: 1080, height: 1024 })
+
+    // captura de pantalla
+    console.log('capturando pantalla del buscador')
+    await page.screenshot({ path: "imagenes/google17.jpg" })
+    
+    await page.type("#APjFqb", "incubadora desafio")
+    console.log("capturando pantalla de búsqueda")
+    await page.screenshot({ path: "imagenes/google18.jpg" })
+
+    //Espere y haga clic en el primer resultado.
+    await page.click(".gNO89b") //google
+    console.log("haciendo clic en buscar")
+    await page.waitForSelector("[data-snc=ih6Jnb_PcjQmf]") //google  esperar la carga del componente q contiene link a
+    console.log('esperando resultado de la búsqueda')
+    await page.screenshot({ path: "imagenes/google19.jpg" }) //
+    console.log("capturando pantalla de resultado")
+
+    // inspeccionar pagina
+    console.log('inspeccionando la página...')
+    const enlaces = await page.evaluate(() => {
+      const elements = document.querySelectorAll(
+        "[data-snc=ih6Jnb_PcjQmf] div div div span a"
+      )
+
+      const links = []
+      for (let element of elements) {
+        links.push(element.href)
+      }
+      return links
+    })
+    console.log("enlaces encontrados: ", enlaces.length) // numero de enlaces
+
+    const registros = []
+    console.log('recorriendo enlaces encontrados...')
+    for (let enlace of enlaces) {
+      await page.goto(enlace)
+      await page.waitForSelector(".carreras-en-que-consiste") // esperar carga de esta clase
+
+      const info = await page.evaluate(() => {
+        const tmp = {}
+        tmp.data1 = document.querySelector(
+          ".carreras-en-que-consiste li"
+        ).innerHTML
+        tmp.data2 = document.querySelector(".detalle-programa h4").innerHTML
+        return tmp
+      })
+      registros.push(info)
+    }
+    console.log('entregando información en Json')
+    console.log("registros: ", registros)
+
+    await browser.close()
+  } catch (error) {
+    console.log("No se pudo crear una instancia del navegador => : ", error)
+    return
   }
-  console.log(books)
-
-  // await page.click(searchResultSelector)
-  // await page.waitForSelector(searchResultSelector)
-
-  // Localice el título completo con una cadena única
-  /*   const textSelector = await page.waitForSelector("text/Customize and automate")
-  const fullTitle = await textSelector?.evaluate((el) => el.textContent) */
-
-  // Imprimir el título completo
-  // console.log('The title of this blog post is "%s".', fullTitle)
-
-  await browser.close()
 })()
 
 /*
- node index
-
+  node index
+  
 */
